59b8cbdfd09d278711dc71b092e7874c695104a8
Lucas-Leal-Oliveira05 <lucas.l.oliveira0509@gmail.com>
2026-06-20 17:27:39 -0300

    Componente Header
    Foi criado o componente Header. Além disso, ja foi criado o arquivo referente à página principal dos usuários (que mostra suas manifestações) e sua rota

--- src/ouvidoria-catolica-app/src/App.tsx
+++ src/ouvidoria-catolica-app/src/App.tsx
@@ -1,5 +1,6 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Login from './login'; // Sua tela de login atual
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./login"; // Sua tela de login atual
+import User from "./user";
 
 // --- CONFIGURAÇÃO DAS ROTAS ---
 export default function App() {
@@ -9,6 +10,7 @@ export default function App() {
         {/* Rota Inicial / Padrão: Sempre carrega o Login */}
         <Route path="/" element={<Login />} />
         <Route path="*" element={<Navigate to="/" replace />} />
+        <Route path="user" element={<User />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/ouvidoria-catolica-app/src/user.tsx
@@ -0,0 +1,8 @@
+//import { useState } from "react";
+import Header from "./components/Header";
+
+export default function User() {
+    return(
+        <Header/>
+    );
+}
